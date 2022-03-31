ee714aeacc30ee4cf06f4de6e8fdee93dd776829
ykhiari <yassine.khiari@outlook.com>
2022-03-31 22:19:53 +0200

    basic server client connection using docker compose

---
+++ client/echo-client.py
@@ -0,0 +1,11 @@
+import socket
+
+HOST = "172.18.0.2"
+PORT = 12345
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
